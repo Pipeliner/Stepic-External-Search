a2f2cefd0c8ddf5c8aa13b5f91fdbbbec1769e3e
Vadim Evard <v.e.evard@gmail.com>
2015-06-20 11:57:39 +0300

    Fucntioning search form

--- search/extsearch/views.py
+++ search/extsearch/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hey search world!")
+    query_string = request.GET['q'] if 'q' in request.GET else ""
+    return render(request, 'extsearch/index.html', {'query_string': query_string, 'search_results': "Fake search results"})
