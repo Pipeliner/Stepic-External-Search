6101fb7796529b7e4f369a77c84629bf2bf552ae
Vadim Evard <v.e.evard@gmail.com>
2015-06-22 16:27:21 +0300

    Functioning Elasticsearch-powered search form for titles

--- search/extsearch/views.py
+++ search/extsearch/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from elasticsearch import Elasticsearch
 
 def index(request):
     query_string = request.GET['q'] if 'q' in request.GET else ""
-    return render(request, 'extsearch/index.html', {'query_string': query_string, 'search_results': "Fake search results"})
+    es = Elasticsearch()
+    elastic_results = es.search(index="stepic", body={"query": {"match": { "title": query_string}}})
+    hits = elastic_results['hits']['hits']
+    response_results = [{'id': hit['_id'], 'title': hit['_source']['title']} for hit in hits]
+    query_results = {'query_string': query_string, 'search_results': response_results}
+    return render(request, 'extsearch/index.html', query_results)
