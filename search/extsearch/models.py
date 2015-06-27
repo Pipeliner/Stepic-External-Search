3e24a45d7262bf35d495ccac27986591d97fe86e
Vadim Evard <v.e.evard@gmail.com>
2015-06-27 23:34:27 +0300

    Make search results clickable links

--- search/extsearch/models.py
+++ search/extsearch/models.py
@@ -7,6 +7,11 @@ class Lesson(models.Model):
     def __str__(self):
         return self.title
 
+    def href(self):
+        base_url = "//stepic.org/lesson/"
+        lesson_url = self.title.replace(" ", "-") + "-" + str(self.id)
+        return base_url + lesson_url
+
 class Step(models.Model):
     lesson = models.ForeignKey(Lesson)
     text = models.CharField(max_length=50000)
--- search/extsearch/views.py
+++ search/extsearch/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from elasticsearch import Elasticsearch
+from .models import Lesson
 
 def index(request):
     query_string = request.GET['q'] if 'q' in request.GET else ""
     es = Elasticsearch()
-    elastic_results = es.search(index="stepic", body={"query": {"match": { "title": query_string}}})
+    elastic_results = es.search(index="stepic", body={"query": {"match": {"title": query_string}}})
     hits = elastic_results['hits']['hits']
-    response_results = [{'id': hit['_id'], 'title': hit['_source']['title']} for hit in hits]
+    response_results = [Lesson.objects.get(pk=hit['_id']) for hit in hits]
     query_results = {'query_string': query_string, 'search_results': response_results}
     return render(request, 'extsearch/index.html', query_results)
